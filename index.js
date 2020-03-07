7e3e4bc148378e9d344799b987f4089ad4a0f39c
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2020-03-08 00:07:48 +0900

    Update file stream

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 'use strict';
 const http = require('http');
+const fs = require('fs');
+const rs = fs.createReadStream('./index.html');
+
 const server = http.createServer((req, res) => {
   const now = new Date();
   console.info('[' + now + '] Requested by ' + req.connection.remoteAddress);
   res.writeHead(200, {
-    'Content-Type': 'text/plain; charset=utf-8'
+    'Content-Type': 'text/html; charset=utf-8'
   });
-
   switch (req.method) {
     case 'GET':
       res.write('GET ' + req.url);
+      rs.pipe(res);
       break;
     case 'POST':
       res.write('POST ' + req.url);
@@ -18,12 +21,12 @@ const server = http.createServer((req, res) => {
         rawData = rawData + chunk;
       }).on('end', () => {
         console.info('[' + now + '] Data posted: ' + rawData);
+        res.end();
       });
       break;
     default:
       break;
   }
-  res.end();
 }).on('error', (e) => {
   console.error('[' + new Date() + '] Server Error', e);
 }).on('clientError', (e) => {
